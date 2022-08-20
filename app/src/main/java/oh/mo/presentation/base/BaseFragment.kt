89696ca63e0d7f93b9a30e9b001d681e710f4276
hyooosong <gythd1998@gmail.com>
2022-08-20 16:13:53 +0900

    [feat] Set BaseActivity / BaseFragment

---
+++ app/src/main/java/oh/mo/presentation/base/BaseFragment.kt
@@ -0,0 +1,38 @@
+package oh.mo.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<T : ViewDataBinding, VM : ViewModel> : Fragment() {
+    lateinit var binding: T
+    abstract val viewModel: VM
+    abstract val layoutResourceId: Int
+
+    abstract fun initStartView()
+    abstract fun initBinding()
+    abstract fun initAfterBinding()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResourceId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+
+        initStartView()
+        initBinding()
+        initAfterBinding()
+    }
+}
