41e988b35f59ee9ebe11bb6c5d32fce8f92425cd
dongk00 <dddongk00@gmail.com>
2022-08-25 16:49:50 +0900

    [feat] set recyclerview

--- app/src/main/java/oh/mo/presentation/home/HomeFragment.kt
+++ app/src/main/java/oh/mo/presentation/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package oh.mo.presentation.home
 
+import androidx.recyclerview.widget.GridLayoutManager
 import oh.mo.R
 import oh.mo.databinding.FragmentHomeBinding
 import oh.mo.presentation.base.BaseFragment
@@ -20,6 +21,26 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     }
 
     override fun initAfterBinding() {
+        setRecyclerView()
+    }
 
+    private fun setRecyclerView() {
+        binding.rvHomeRecommendationsFeed.apply {
+            adapter = RecommendationsFeedAdapter()
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            addItemDecoration(RecommendationsFeedDecoration(requireContext()))
+        }
+
+        //dummy data
+        val list = mutableListOf<String>()
+            .also {
+                for (i in 1..7) {
+                    it.add("Recommendation $i")
+                }
+            }
+
+        (binding.rvHomeRecommendationsFeed.adapter as RecommendationsFeedAdapter)
+            .differ
+            .submitList(list)
     }
 }
