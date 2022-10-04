8332f93266fa33c6be41c289e6b1a8f0c8fbffdb
dongk00 <dddongk00@gmail.com>
2022-10-04 22:56:16 +0900

    [layout] SignUpFragment

--- app/src/main/java/oh/mo/presentation/signin/SignInFragment.kt
+++ app/src/main/java/oh/mo/presentation/signin/SignInFragment.kt
@@ -1,10 +1,5 @@
 package oh.mo.presentation.signin
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import oh.mo.R
 import oh.mo.databinding.FragmentSignInBinding
