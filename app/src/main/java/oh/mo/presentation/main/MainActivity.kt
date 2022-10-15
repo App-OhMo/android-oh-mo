0f1d6c7fdcdb4de794c3b6196b2a7318cfef0191
유나연 <yny026@gmail.com>
2022-10-15 16:50:56 +0900

    layout: 검색순위부분 레이아웃 수정

--- app/src/main/java/oh/mo/presentation/main/MainActivity.kt
+++ app/src/main/java/oh/mo/presentation/main/MainActivity.kt
@@ -3,6 +3,8 @@ package oh.mo.presentation.main
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,19 +15,20 @@ import oh.mo.presentation.base.BaseActivity
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it)}) {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setBottomNavigation()
+        setInitToolbarIcon()
         setTheme(R.style.statusBarThemeBlue)
     }
 
-
     private fun setBottomNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fm_main_nav_host)
-        val navController = navHostFragment?.findNavController()
-        binding.navBar.setupWithNavController((navController ?: throw NullPointerException()))
+        navController = navHostFragment?.findNavController() ?: throw NullPointerException()
+        binding.navBar.setupWithNavController((navController))
 
 
         navController.addOnDestinationChangedListener { _, dest, _ ->
@@ -50,4 +53,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
             }
         }
     }
+
+    private fun setInitToolbarIcon() {
+        binding.ivHomeSearch.setOnClickListener{
+            navController.navigate(R.id.action_home_to_search)
+        }
+
+        binding.ivHomePosting.setOnClickListener() {
+            //포스팅 아이콘 로직
+        }
+    }
+
 }
--- app/src/main/java/oh/mo/presentation/search/SearchFragment.kt
+++ app/src/main/java/oh/mo/presentation/search/SearchFragment.kt
@@ -1,5 +1,20 @@
 package oh.mo.presentation.search
 
-class SearchFragment {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import oh.mo.databinding.FragmentSearchBinding
+import oh.mo.presentation.base.BaseFragment
+
+class SearchFragment: BaseFragment<FragmentSearchBinding>() {
+
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSearchBinding {
+        return FragmentSearchBinding.inflate(inflater, container, false)
+    }
 
 }
