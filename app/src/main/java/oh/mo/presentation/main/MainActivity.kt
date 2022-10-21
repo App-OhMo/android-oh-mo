ce847b6b889f562801828d3515ce2674fc1e4a47
유나연 <yny026@gmail.com>
2022-10-21 17:39:24 +0900

    layout: 검색프래그먼트 사용 시 툴바제거

--- app/src/main/java/oh/mo/presentation/main/MainActivity.kt
+++ app/src/main/java/oh/mo/presentation/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package oh.mo.presentation.main
 
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.core.view.isGone
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -40,6 +42,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
                         title = "프로필"
                         setTitleTextColor(resources.getColor(R.color.black_1a))
                         setBackgroundColor(resources.getColor(R.color.white))
+                        binding.tbMain.visibility = View.VISIBLE
                     }
                 }
                 R.id.fragment_home -> {
@@ -48,6 +51,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
                         title = "오늘의 날씨"
                         setTitleTextColor(resources.getColor(R.color.white))
                         setBackgroundColor(resources.getColor(R.color.blue_64))
+                        binding.tbMain.visibility = View.VISIBLE
                     }
                 }
             }
@@ -57,6 +61,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
     private fun setInitToolbarIcon() {
         binding.ivHomeSearch.setOnClickListener{
             navController.navigate(R.id.action_home_to_search)
+            binding.tbMain.visibility = View.GONE
         }
 
         binding.ivHomePosting.setOnClickListener() {
