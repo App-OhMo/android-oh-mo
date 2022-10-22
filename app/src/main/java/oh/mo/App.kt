8da20d49419b1b12160f5225bc13129f2254e648
유나연 <yny026@gmail.com>
2022-10-22 19:34:40 +0900

    feat: 클래스에서 context사용 위한 코드 추가

--- app/src/main/java/oh/mo/App.kt
+++ app/src/main/java/oh/mo/App.kt
@@ -1,7 +1,19 @@
 package oh.mo
 
 import android.app.Application
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+
+    companion object{
+        lateinit var instance: App
+        private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
